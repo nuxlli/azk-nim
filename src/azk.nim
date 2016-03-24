566e9a52e69955e777aaa6405715e4a6f285d2bc
Everton Ribeiro <nuxlli@gmail.com>
2016-03-23 21:56:41 -0300

    Adding modules and refactoring to load real Azkfile.js

--- src/azk.nim
+++ src/azk.nim
@@ -2,4 +2,16 @@
 import duktape
 
 var dt = new Duktape()
-dt.eval("require('Azkfile.js');")
+dt.eval("""
+  var pp = require('./modules/utils.js').prettyPrint;
+  try {
+    var _systems = {}
+    function systems(data) { _systems = data; }
+    function path(data) { return data; }
+    function sync(data) { return data; }
+    require('./Azkfile.js');
+    pp("Azkfile", _systems);
+  } catch (e) {
+    pp(e);
+  }
+""")
